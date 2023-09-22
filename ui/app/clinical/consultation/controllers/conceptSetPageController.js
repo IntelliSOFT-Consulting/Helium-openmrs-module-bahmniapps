7957a69eb7732be8d041d0452225db3ada69bed7
daviemukungi <mukungidavid@gmail.com>
2023-09-22 12:12:26 +0300

    Remove form user roles from forms

--- ui/app/clinical/consultation/controllers/conceptSetPageController.js
+++ ui/app/clinical/consultation/controllers/conceptSetPageController.js
@@ -18,57 +18,6 @@ angular.module('bahmni.clinical')
             var fields = ['uuid', 'name:(name,display)', 'names:(uuid,conceptNameType,name)'];
             var customRepresentation = Bahmni.ConceptSet.CustomRepresentationBuilder.build(fields, 'setMembers', numberOfLevels);
             var allConceptSections = [];
-            var historyAndExaminitionsUUID = "c393fd1d-3f10-11e4-adec-0800271c1b75";
-            var vitalsUUID = "c36a7537-3f10-11e4-adec-0800271c1b75";
-            var SecondVitals = "c38669d1-3f10-11e4-adec-0800271c1b75";
-            var Obstetrics = "c44f4ba3-3f10-11e4-adec-0800271c1b75";
-            var Gynaecology = "c45d1339-3f10-11e4-adec-0800271c1b75";
-            var DischargeSummary = "c389d0ea-3f10-11e4-adec-0800271c1b75";
-            var TuberclosisIntakeTemplate = "e95b2fd4-8956-474e-8e0e-630932f6ecee";
-            var TuberclosisFolloupTemplate = "4276dcf6-0f21-4910-bac3-87ddc94f88c9";
-            var ANC = "18351dd1-078e-456c-8c23-915b16e9dfaf";
-            var DiabetesIntake = "ec27d7a3-c9bc-44ee-80b9-b998db3e278a";
-            var DiabetesProgress = "957f4eb6-1193-4303-aab2-add4e966d1b1";
-            var HypertensionIntake = "b98107dc-fb15-499c-8bb2-41b9a7f16c28";
-            var HypertensionProgress = "da8e93dd-f59c-4c1d-b4c7-7bcd48c49e95";
-            var Malaria = "58370446-3439-4007-a6bc-4d6687c8d354";
-            var DeathNote = "208c3975-77f7-43bb-aca3-b5ffa9c1bfcc";
-            var DeliveryNote = "199720f7-eb6e-491f-ac8e-b1eca6faee0d";
-            var ChildhoodIllnessbelow2months = "68956dcb-e407-4fd6-ac5a-b243c57a3a28";
-            var ChildhoodIllnessBetween = "7963985f-5740-4b35-953d-c48a987cd72e";
-            var IMAMProgram = "532ae9c8-9b95-4b7f-b22e-9fffe0810d7c";
-            var Nutrition = "d2e964c9-c546-4444-8fca-5179a22c8b31";
-            var HIVTestingAndCounselingIntakeTemplate = "6bfd85ce-22c8-4b54-af0e-ab0af24240e3";
-            var HIVTreatmentAndCareIntakeTemplate = "03a7cac1-2562-4151-9f3e-8f07c6c94731";
-            var HIVTreatmentandCareProgressTemplate = "746818ac-65a0-4d74-9609-ddb2c330a31b";
-            var OpioidSubstitutionTherapyIntakeTemplate = "63a42108-5647-4884-a4f3-f8e74d7e9f70";
-            var OpportunisticInfectionTemplate = "f8ce62bf-315a-47ba-b085-b896f00e4c31";
-            var PreventionofMotherChildTransmissionIntakeTemplate = "c1eda433-7d3d-4c58-901b-587a7ddbc870";
-            var SafeAbortion = "1e55759a-04ca-4c6f-9d26-d7fbf1497e34";
-            var PNC = "ef29ac41-47c9-4f4d-9f53-0d4e797b9fef";
-            var FamilyPlanningTemplate = "8d62f476-8b96-4dde-8452-2d4d5742a817";
-            var PostDeliveryFamilyPlanningChecklist = "981ee726-d61d-4e85-b411-99e7682f75a4";
-            var ChronicKidneyDeseaseIntake = "a1a77be7-8cfb-4c7c-957e-979285c1ccde";
-            var ChronicKidneyDiseaseProgress = "b84304cb-5cb2-412f-ab7c-0df9cef91195";
-            var COPDIntake = "5a0157fb-73e4-4094-9480-a3e35e6e0a43";
-            var COPDProgress = "0f2daa04-8c51-4aa7-97e9-83cece626a36";
-            var AsthmaProgress = "fe0b2622-dc21-4c37-841b-36c234329cb0";
-            var CongestiveHeartFailureProgress = "496e596d-b703-4f06-9303-d6f3b2ebf8e5";
-            var CoronaryArteryDiseaseProgress = "5728d998-b57d-4d2f-9d78-c742bc56a7bd";
-            var RheumaticHeartDiseaseProgress = "40c461f7-1c27-4ac8-8ef5-68ad8f6a458a";
-            var SeizureDisorderProgress = "67a7b81a-2944-4da0-8b4e-99c808d4f387";
-            var StrokeOPD = "c2d65529-ad8d-4ca0-a7f8-417c25774618";
-            var LeprosyTemplate = "bdc9b494-b53f-4799-83ce-5872efe81d44";
-            var MentalHealth = "fec90bd6-5d2a-4577-8247-4cae93316fc1";
-            var OrthopaedicExamination = "f5d4b684-05d0-427f-b32b-ca9c65430727";
-            var ECGNotes = "3f6c1dbc-1841-494a-a548-32a489988732";
-            var ERGeneralNotes = "d262e419-1b95-45f2-91c8-9e70e237795a";
-            var OperativeNotes = "f3d7969c-e9c7-4c7a-9b4f-278617212c3f";
-            var USGNotes = "285604a9-6e8b-4029-9af6-947a1f0a2e17";
-            var ProcedureNotes = "930adbff-e6c9-44b6-a717-b9dfc678dd9e";
-            var TraumaNotes = "66b8168a-2284-4f48-b134-8131c0d65cfe";
-            var BreastCancerFollowupTemplate = "198d7651-e7ee-4693-bcba-154a5d005741";
-            var BreastCancerIntakeTemplate = "eba06fb1-846a-44bf-9885-322f6aa28b0e";
 
             var init = function () {
                 if (!($scope.allTemplates !== undefined && $scope.allTemplates.length > 0)) {
@@ -77,30 +26,6 @@ angular.module('bahmni.clinical')
                         v: "custom:" + customRepresentation
                     }).then(function (response) {
                         var allTemplates = response.data.results[0].setMembers;
-                        var privileges = $rootScope.currentUser.privileges;
-                        for (var i = privileges.length - 1; i >= 0; i--) {
-                            // console.log("Current user privilege::", privileges[i].name);
-                            var userPrivilege = privileges[i].name;
-                            if (userPrivilege == "Doctor") {
-                                for (var i = allTemplates.length - 1; i >= 0; i--) {
-                                    if (allTemplates[i].uuid == historyAndExaminitionsUUID || allTemplates[i].uuid == vitalsUUID) {
-                                        allTemplates.splice(i, 1);
-                                    }
-                                }
-                            } else if (userPrivilege == "Nurse") {
-                                for (var i = allTemplates.length - 1; i >= 0; i--) {
-                                    if (allTemplates[i].uuid === SecondVitals || allTemplates[i].uuid === Obstetrics || allTemplates[i].uuid == Gynaecology || allTemplates[i].uuid == Obstetrics || allTemplates[i].uuid == DischargeSummary || allTemplates[i].uuid == TuberclosisIntakeTemplate || allTemplates[i].uuid == TuberclosisFolloupTemplate || allTemplates[i].uuid == ANC ||
-                                        allTemplates[i].uuid === DiabetesIntake || allTemplates[i].uuid === DiabetesProgress || allTemplates[i].uuid === HypertensionIntake || allTemplates[i].uuid == HypertensionProgress || allTemplates[i].uuid == Malaria || allTemplates[i].uuid == DeathNote || allTemplates[i].uuid == DeliveryNote ||
-                                        allTemplates[i].uuid === ChildhoodIllnessbelow2months || allTemplates[i].uuid === ChildhoodIllnessBetween || allTemplates[i].uuid == IMAMProgram || allTemplates[i].uuid == Nutrition || allTemplates[i].uuid == HIVTestingAndCounselingIntakeTemplate || allTemplates[i].uuid == HIVTreatmentAndCareIntakeTemplate || allTemplates[i].uuid == HIVTreatmentandCareProgressTemplate || allTemplates[i].uuid == OpioidSubstitutionTherapyIntakeTemplate ||
-                                        allTemplates[i].uuid === OpportunisticInfectionTemplate || allTemplates[i].uuid === PreventionofMotherChildTransmissionIntakeTemplate || allTemplates[i].uuid == SafeAbortion || allTemplates[i].uuid == PNC || allTemplates[i].uuid == FamilyPlanningTemplate || allTemplates[i].uuid == PostDeliveryFamilyPlanningChecklist || allTemplates[i].uuid == ChronicKidneyDeseaseIntake || allTemplates[i].uuid == ChronicKidneyDiseaseProgress || allTemplates[i].uuid == COPDIntake ||
-                                        allTemplates[i].uuid === COPDProgress || allTemplates[i].uuid === AsthmaProgress || allTemplates[i].uuid == CongestiveHeartFailureProgress || allTemplates[i].uuid == CoronaryArteryDiseaseProgress || allTemplates[i].uuid == StrokeOPD || allTemplates[i].uuid == LeprosyTemplate ||
-                                        allTemplates[i].uuid === RheumaticHeartDiseaseProgress || allTemplates[i].uuid === SeizureDisorderProgress || allTemplates[i].uuid == MentalHealth || allTemplates[i].uuid == OrthopaedicExamination || allTemplates[i].uuid == ECGNotes || allTemplates[i].uuid == ERGeneralNotes || allTemplates[i].uuid == OperativeNotes || allTemplates[i].uuid == ProcedureNotes ||
-                                        allTemplates[i].uuid === USGNotes || allTemplates[i].uuid == TraumaNotes || allTemplates[i].uuid === BreastCancerFollowupTemplate || allTemplates[i].uuid == BreastCancerIntakeTemplate) {
-                                        allTemplates.splice(i, 1);
-                                    }
-                                }
-                            }
-                        }
                         createConceptSections(allTemplates);
                         if ($state.params.programUuid) {
                             showOnlyTemplatesFilledInProgram();
